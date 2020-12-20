ac323f32f887932fdb71a4441ea1b3673ea62d9f
GeorgeOfSystem <49001431+GeorgeOfSystem@users.noreply.github.com>
2020-12-20 16:58:53 -0400

    Update app.py

--- app.py
+++ app.py
@@ -1,16 +1,19 @@
 from flask import Flask, jsonify, request
 from firebase import firebase
-import json
 
-firebase = firebase.FirebaseApplication("https://coderangersproject.firebaseio.com/", None)
+firebase = (firebase.FirebaseApplication
+            ("https://coderangersproject.firebaseio.com/", None))
 
-app = Flask(__name__)
+app = Flask(_name_)
+
 
 @app.route('/')
 def index():
-    return 'OPTIONS:<br><b>GET</b> <br> /formularios <br> /formularios/{ownerId}'
+    return ('OPTIONS:<br><b>GET</b>'
+            ' <br> /formularios <br> /formularios/{ownerId}')
 
-#GET
+
+# GET
 @app.route('/formularios/<string:oid>')
 def getProductsByOwnerId(oid):
     forms = firebase.get('/formularios', '')
@@ -18,37 +21,41 @@ def getProductsByOwnerId(oid):
     for f in forms:
         if forms[f]['ownerId'] == oid:
             result.append([f, forms[f]])
-            
+
     if len(result) == 0:
-        return jsonify({"message":"Usuario no existente"})
-    return  jsonify(result)
+        return jsonify({"message": "Usuario no existente"})
+    return jsonify(result)
+
 
 @app.route('/formularios')
 def getForms():
     return firebase.get('/formularios', '')
-    
-    
-#POST    
-@app.route('/formularios', methods = ['POST'])
+
+
+# POST
+@app.route('/formularios', methods=['POST'])
 def addForm():
     firebase.post('/formularios', request.json)
-    return jsonify({"message":"Recieved"})
+    return jsonify({"message": "Recieved"})
 
-#PUT
-@app.route('/formularios/<string:fid>', methods = ['PUT'])
+
+# PUT
+@app.route('/formularios/<string:fid>', methods=['PUT'])
 def updateForm(fid):
-    firebase.put('/formularios',fid, request.json)
-    return jsonify({"message":"Updated"})
+    firebase.put('/formularios', fid, request.json)
+    return jsonify({"message": "Updated"})
 
-#DELETE
-@app.route('/formularios/<string:fid>', methods = ['DELETE'])
+
+# DELETE
+@app.route('/formularios/<string:fid>', methods=['DELETE'])
 def deleteForm(fid):
     firebase.delete('/formularios', fid)
-    
-    if fid != "":
-        return jsonify({"message":"Deleted"})
-    
-    return jsonify({"message":"No Form Found"})
 
-if __name__ == '__main__':
+    if fid != "":
+        return jsonify({"message": "Deleted"})
+
+    return jsonify({"message": "No Form Found"})
+
+
+if _name_ == '_main_':
     app.run(debug=True, port=5000)
